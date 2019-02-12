eb894f09ba488fd75db90453aef28862e84e2ef9
Mika Bessel <mikabessel@googlemail.com>
2019-02-12 13:34:24 +0200

    angular material css fixed

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+// import * as swal from 'sweetalert2';
+
+@Component({
+  selector: 'test',
+  templateUrl: './test.component.html',
+})
+export class TestComponent {
+
+  constructor( ) {}
+
+
+}
